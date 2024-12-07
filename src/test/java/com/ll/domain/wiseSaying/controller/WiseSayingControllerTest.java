ba3698094f88b8b91b66d7f7723025b3cda9cf14
TTaiJin <taeparis980110@gmail.com>
2024-12-07 17:07:35 +0900

    step3: if add wiseSaying, output wiseSayingNumber R

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WiseSayingControllerTest {
     @Test
     @DisplayName("2단계: 명언, 작가 입력받기")
-    public void t1() {
+    public void t2() {
         String output = AppTest.run("""
                 등록
                 현재를 사랑하라.
@@ -18,5 +18,17 @@ public class WiseSayingControllerTest {
 
         assertThat(output).contains("명언 : ")
                 .contains("작가 : ");
+    }
+
+    @Test
+    @DisplayName("3단계: 등록시 생성된 명언번호 노출")
+    public void t3() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(output).contains("1번 명언이 등록되었습니다.");
     }
 }
